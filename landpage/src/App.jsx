d1ae4ebde9d1c87efd36f82efdc892442f21267d
Avinashraj21 <avinashselvi214@gmail.com>
2024-08-09 19:17:23 +0530

    first commit

---
+++ landpage/src/App.jsx
@@ -0,0 +1,27 @@
+import Navigation from "./component/Navigation"
+import Masthead from "./component/Masthead"
+import IconsGrid from "./component/IconsGrid"
+import ImageShowcases from "./component/ImageShowcases"
+import Testimonials from "./component/Testimonials"
+import CalltoAction from "./component/CalltoAction"
+import Footer from "./component/Footer"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navigation />
+      <Masthead />
+      <IconsGrid/>
+      <ImageShowcases/>
+      <Testimonials/>
+      <CalltoAction/>
+      <Footer/>
+
+
+    </>
+  )
+}
+
+export default App
